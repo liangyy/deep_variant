e4d2c59b869e75f203a4b88c9318dc9fea1c2209
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-04-18 14:37:20 -0500

    added motif acitvation analysis (not debugged)

--- motif_analysis/README.md
+++ motif_analysis/README.md
@@ -1,3 +1,9 @@
+# Overview
+
+This pipeline contains two main steps:
+1. extract a subset of sequences of interest for downstream analysis
+2. take the subset sequences and generate the patterns of interest
+
 # Environment
 
 ```
@@ -10,3 +16,10 @@ To get a set of selected sequences for downstream analysis:
 ```
 $ snakemake data/E081/selected_positive.hdf5
 ```
+
+# TODO
+
+1. implement `mutagensis` [link](modules/seq2pattern/_mutagensis.snakemake)
+2. implement `sliding_window` [link](modules/seq2pattern/_sliding_window.snakemake)
+3. implement `motif_gradient` [link](modules/seq2pattern/_motif_gradient.snakemake)
+4. implement `motif_activation` [link](modules/seq2pattern/_motif_activation.snakemake)
---
+++ motif_analysis/scripts/motif_activation.py
@@ -0,0 +1,30 @@
+import argparse
+parser = argparse.ArgumentParser(prog='motif_activation.py', description='''
+	Given sequences and model and output the activation of convolutional layer
+    (by layer index), where the spatial information is collapsed.
+	''')
+parser.add_argument('--model')
+parser.add_argument('--input')
+parser.add_argument('--output')
+parser.add_argument('--idx', type=int)
+args = parser.parse_args()
+
+from keras.models import load_model
+import numpy as np
+import sys
+if 'scripts/' not in sys.path:
+    sys.path.insert(0, 'scripts/')
+import my_python
+import feather
+import pandas as pd
+
+x = my_python.getData(args.input, 'x')
+
+model = load_model(args.model)
+from keras.models import Model
+model_intermediate = Model(inputs=model.layers[0].input, outputs=model.layers[args.idx].output)
+activation = model_intermediate.predict(x, verbose=1)
+activation = np.max(axis=1)
+
+total_table = pd.DataFrame(activation, columns=[ 'Motif.' + str(i) for i in range(ctivation.shape[1]) ])
+feather.write_dataframe(total_table, args.output)
