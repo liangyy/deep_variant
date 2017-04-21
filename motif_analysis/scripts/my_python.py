715964333b36008c96c643f30cfe7a27cd4129a1
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-04-21 15:54:45 -0500

    motif grad 1

--- motif_analysis/scripts/my_python.py
+++ motif_analysis/scripts/my_python.py
@@ -40,4 +40,31 @@ def saveData(hdf5file, name, data):
     f = h5py.File(hdf5file, 'w')
     f.create_dataset(name, data=data)
     f.close()
-    
+def getGradient(model, layer_idx, sample, label_idx):
+    # ATTENTION
+    # layer_idx and label_idx should be 0-based
+    from keras.models import load_model
+    from keras.layers import Dense, Activation
+    model = load_model(model)
+    noutput = model.layers[-1].output_shape[1]
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    import numpy as np
+    linear = np.zeros((noutput, 1))
+    linear[label_idx, 0] = 1
+    bias = 0
+    model.layers[-2].set_weights((linear, bias))
+    gradients = model.optimizer.get_gradients(model.model.total_loss, model.layers[layer_idx].output)
+    import keras.backend as K
+    input_tensors = [model.inputs[0], # input data
+                     model.model.sample_weights[0], # how much to weight each sample by
+                     model.targets[0], # labels
+                     K.learning_phase(), # train or test mode
+    ]
+    get_gradients = K.function(inputs=input_tensors, outputs=gradients)
+    inputs = [[sample[0]], # X
+          [1], # sample weights
+          [sample[1]], # y
+          0 # learning phase in TEST mode
+    ]
+    return get_gradients(inputs)[1]
