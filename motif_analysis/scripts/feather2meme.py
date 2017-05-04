e3f2495a6c1153bd68449cb6ec8d9d82dee5ec9f
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-05-04 15:30:33 -0500

    added motif analysis - searching database

--- motif_analysis/scripts/feather2meme.py
+++ motif_analysis/scripts/feather2meme.py
@@ -3,7 +3,7 @@ parser = argparse.ArgumentParser(prog='feather2meme.py', description='''
 	Given PWM in feather format, output the MEME format ready for motif
     analysis
 	''')
-parser.add_argument('--feather')
+parser.add_argument('--feathers', nargs = '+')
 parser.add_argument('--output')
 
 args = parser.parse_args()
@@ -13,13 +13,14 @@ import feather
 import pandas as pd
 
 o = open(args.output, 'w')
-m = feather.read_dataframe(args.feather)
-width = m.shape[1]
-m = m.T[[0, 2, 1, 3]] # reorder bases to A, C, G, T
-headline = 'MOTIF {motif} {motif}'.format(motif = args.feather)
-matheader = 'letter-probability matrix: alength= 4'
-mat = m.to_string(index = False, header = False)
-o.write(headline + '\n')
-o.write(matheader + '\n')
-o.write(mat)
+for motif in args.feathers:
+	m = feather.read_dataframe(motif)
+	width = m.shape[1]
+	m = m.T[[0, 2, 1, 3]] # reorder bases to A, C, G, T
+	headline = 'MOTIF {motif} {motif}'.format(motif = motif)
+	matheader = 'letter-probability matrix: alength= 4'
+	mat = m.to_string(index = False, header = False)
+	o.write(headline + '\n')
+	o.write(matheader + '\n')
+	o.write(mat)
 o.close()
