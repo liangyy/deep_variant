c7605f39c4d0336f3ae5f6bcca7b176ef86b8703
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-05-07 09:48:03 -0500

    debugging

--- gc_content/scripts/gc.py
+++ gc_content/scripts/gc.py
@@ -1,5 +1,5 @@
 import argparse
-parser = argparse.ArgumentParser(prog='curve.py', description='''
+parser = argparse.ArgumentParser(prog='gc.py', description='''
     Given x, y, y_pred, output a feather file containing GC content,
     y, and y_pred per sequence
         ''')
@@ -12,11 +12,11 @@ parser.add_argument('--name_yp')
 parser.add_argument('--idx_y', type=int, help='1-based')
 parser.add_argument('--idx_yp', type=int, help='1-based')
 parser.add_argument('--out')
-parser.add_argument('--xdouble')
-parser.add_argument('--ydouble')
-parser.add_argument('--ypdouble')
-parser.add_argument('--yremove', help='1-based: remove [start]-[end]')
-parser.add_argument('--ypremove')
+parser.add_argument('--doublex')
+parser.add_argument('--doubley')
+parser.add_argument('--doubleyp')
+parser.add_argument('--removey', help='1-based: remove [start]-[end]')
+parser.add_argument('--removeyp')
 
 args = parser.parse_args()
 
@@ -32,21 +32,21 @@ import feather
 
 x = my_python.getData(args.x, args.name_x)
 x = x[:, :, :2]
-if args.xdouble == 'Yes':
+if args.doublex == 'Yes':
     x = x[:int(x.shape[0] / 2)]
 gc = np.sum(x[:,:,1:3], axis = (1,2)) / x.shape[1]
 
-y = my_python.getData(args.ypath, args.yname)
+y = my_python.getData(args.y, args.name_y)
 if y.ndim == 2:
     y = y[:, args.idx_y - 1]
-if args.ydouble == 'Yes':
+if args.doubley == 'Yes':
     if y.shape[0] % 2 == 1:
         my_python.eprint('y dim is not mod 2 zero. Cannot use double mode. Exit')
         sys.exit()
     ny = int(y.shape[0] / 2)
     y = y[:ny]
-if args.yremove != 'None':
-    temp = args.yremove.split('-')
+if args.removey != 'None':
+    temp = args.removey.split('-')
     start = int(temp[0]) - 1
     end = int(temp[1])
     y = np.hstack((y[:start], y[end:]))
@@ -54,14 +54,14 @@ if args.yremove != 'None':
 yp = my_python.getData(args.yp, args.name_yp)
 if yp.ndim == 2:
     yp = yp[:, args.idx_yp - 1]
-if args.ypdouble == 'Yes':
+if args.doubleyp == 'Yes':
     if yp.shape[0] % 2 == 1:
         my_python.eprint('y_pred dim is not mod 2 zero. Cannot use double mode. Exit')
         sys.exit()
     nyp = int(yp.shape[0] / 2)
     yp = (yp[:nyp] + yp[nyp:]) / 2
-if args.ypremove != 'None':
-    temp = args.ypremove.split('-')
+if args.removeyp != 'None':
+    temp = args.removeyp.split('-')
     start = int(temp[0]) - 1
     end = int(temp[1])
     yp = np.hstack((yp[:start], yp[end:]))
@@ -70,5 +70,5 @@ if yp.shape[0] != y.shape[0]:
     sys.exit()
 
 
-table = pd.DataFrame({ 'GC.Content' : gc, 'y' : y, 'y.predict' : y_pred})
+table = pd.DataFrame({ 'GC.Content' : gc, 'y' : y, 'y.predict' : yp})
 feather.write_dataframe(table, args.out)
