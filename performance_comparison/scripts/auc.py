e9806d1b6f43076eb9b3285efe985f22adc98bb1
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-05-15 11:18:55 -0500

    debuged script

--- performance_comparison/scripts/auc.py
+++ performance_comparison/scripts/auc.py
@@ -13,10 +13,10 @@ import feather
 
 data = feather.read_dataframe(args.curve)
 labels = pd.unique(data['data'])
-sequences = pd.unqiue(data['info'])
-curves = [ 'ROC', 'PR' ]
-xy = { 'ROC': {'x': 'fpr', 'y': 'tpr'},
-        'PR': {'x': 'recall', 'y': 'precision'} }
+sequences = pd.unique(data['info'])
+curves = [ 'roc', 'pr' ]
+xy = { 'roc': {'x': 'fpr', 'y': 'tpr'},
+        'pr': {'x': 'recall', 'y': 'precision'} }
 
 labels_out = []
 sequence_out = []
@@ -28,11 +28,12 @@ for label in labels:
     for sequence in sequences:
         data_sequence = data_label[data_label['info'] == sequence]
         for curve in curves:
+            print('{label} - {sequence} - {curve}'.format(label=label, sequence=sequence, curve=curve))
             data_curve = data_sequence[data_sequence['type'] == curve]
             score = metrics.auc(data_curve[xy[curve]['x']], data_curve[xy[curve]['y']])
             labels_out.append(label)
             sequence_out.append(sequence)
             curve_out.append(curve)
             score_out.append(score)
-table = pd.DataFrame({ 'data' : data_out, 'info' : sequence_out, 'curve' : curve_out, 'score': score_out})
+table = pd.DataFrame({ 'data' : labels_out, 'info' : sequence_out, 'curve' : curve_out, 'score': score_out})
 feather.write_dataframe(table, args.out)
