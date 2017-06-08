9f836e6b44dfc6ee03244db6904fab12100ad053
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-06-08 12:16:43 -0500

    added scripts to extract sequence

---
+++ motif_analysis/scripts/signal2seq.py
@@ -0,0 +1,87 @@
+import argparse
+parser = argparse.ArgumentParser(prog='signal2seq.py', description='''
+	Take the output of sliding_window_signal2region.R, extract sequence
+    accordingly (merge or not merge the selected overlaping sequences)
+    and save as table (seq_id, start_pos, end_pos, sequence)
+''')
+parser.add_argument('--position')
+parser.add_argument('--sequence')
+parser.add_argument('--mask_size', type=int)
+parser.add_argument('--merge', type=int, help='''
+    set --merge as 1 if want to merge ovelaps
+''')
+parser.add_argument('--out')
+args = parser.parse_args()
+
+import numpy as np
+import h5py
+import sys
+if 'scripts/' not in sys.path:
+    sys.path.insert(0, 'scripts/')
+import my_python
+import pandas as pd
+
+alphabet_dic = { 0: 'A', 1: 'G', 2: 'C', 3: 'T'}
+col_dic = { 'seq_id': 0, 'pos_id': 1 }
+position = np.loadtxt(args.position, skiprows=1)
+x_new = h5py.File(args.sequence, 'r')
+name = list(x_new.keys())[0]
+sequence = x_new[name][()]
+x_new.close()
+
+num_of_seq_per_seq = my_python.numSeqPerSeq(x_new.shape[1], args.mask_size)
+
+seq_out = []
+start_out = []
+end_out = []
+char_out = []
+if args.merge == 1:
+    start = position[0][col_dic['pos_id']] # 1-based
+    end = position[0][col_dic['pos_id']] + args.mask_size # 1-based, not include
+    seq_memory = position[0][col_dic['seq_id']]
+    for i in range(1, position.shape[0] + 1):
+        if i < position.shape[0]:
+            seq = position[i][col_dic['seq_id']]
+            pos = position[i][col_dic['pos_id']]
+            if end - pos >= args.mask_size / 2 and seq == seq_memory:
+                end = pos + args.mask_size
+            else:
+                seq_out.append(seq_memory)
+                start_out.append(start)
+                end_out.append(end)
+                extracted = sequence[seq_memory * num_of_seq_per_seq - 1][start - 1 : end - 1, :]
+                ex_seq = ''
+                for j in range(extracted.shape[0]):
+                    num = extracted[j,:].argmax()
+                    ex_seq = ex_seq + alphabet_dic[num]
+                start = pos
+                end = pos + args.mask_size
+                seq_memory = seq
+                char_out.append(ex_seq)
+        else:
+            seq_out.append(seq_memory)
+            start_out.append(start)
+            end_out.append(end)
+            extracted = sequence[seq_memory * num_of_seq_per_seq - 1][start - 1 : end - 1, :]
+            ex_seq = ''
+            for j in range(extracted.shape[0]):
+                num = extracted[j,:].argmax()
+                ex_seq = ex_seq + alphabet_dic[num]
+            char_out.append(ex_seq)
+elif args.merge != 1:
+    for i in range(position.shape[0]):
+        seq = position[i][col_dic['seq_id']]
+        pos = position[i][col_dic['pos_id']]
+        start = pos
+        end = pos + args.mask_size
+        extracted = sequence[seq * num_of_seq_per_seq - 1][start - 1 : end - 1, :]
+        ex_seq = ''
+        for j in range(extracted.shape[0]):
+            num = extracted[j,:].argmax()
+            ex_seq = ex_seq + alphabet_dic[num]
+        seq_out.append(seq)
+        start_out.append(start)
+        end_out.append(end)
+        char_out.append(ex_seq)
+total_table = pd.DataFrame({ 'seq_id': seq_out, 'start_pos': start_out, 'end_pos': end_out, 'sequence': char_out})
+total_table.to_csv(args.out, compression='gzip', sep='\t', index=False)
---
+++ motif_analysis/scripts/sliding_window_signal2region.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+library("optparse")
+library(feather)
+
+option_list = list(
+    make_option(c("-f", "--file"), type="character", default=NULL, 
+                help="dataset file name", metavar="character"),
+    make_option(c("-o", "--out"), type="character", default="out.txt", 
+                help="output file name [default= %default]", metavar="character"),
+    make_option(c('-m', '--mode'), type='character', default='positive',
+                help='match the mode with the input file (positive|negative)', metavar='character'),
+    make_option(c('-q', '--quantile'), type='double', default=0.0001)
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+
+pos <- read_feather(opt$file)
+if(opt$mode == 'positive'){
+    threshold <- quantile(unlist(pos), opt$quantile)
+    seq <- c()
+    position <- c()
+    for(x in 1 : nrow(pos)){
+        signal <- pos[x,]
+        pass.idx <- which(signal <= threshold)
+        seq <- c(seq, rep(x, length(pass.idx)))
+        position <- c(position, pass.idx)
+    }
+}else if(opt$mode == 'negative'){
+    threshold <- quantile(unlist(pos), 1 - opt$quantile)
+    seq <- c()
+    position <- c()
+    for(x in 1 : nrow(pos)){
+        signal <- pos[x,]
+        pass.idx <- which(signal >= threshold)
+        seq <- c(seq, rep(x, length(pass.idx)))
+        position <- c(position, pass.idx)
+    }
+}
+
+out <- data.frame(seq_id = seq, pos_id = position)
+write.table(out, file = opt$out, quote = F, row.names = F, col.names = T)
