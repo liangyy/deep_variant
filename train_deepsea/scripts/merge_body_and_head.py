feb05c74e8a72bd09271a6a5a58698b5fbb71665
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-06-29 21:47:58 -0500

    debugged‘

--- train_deepsea/scripts/merge_body_and_head.py
+++ train_deepsea/scripts/merge_body_and_head.py
@@ -14,6 +14,8 @@ if 'scripts/' not in sys.path:
     sys.path.insert(0, 'scripts/')
 import my_python
 from keras.models import load_model
+from keras.models import Model
+
 body = load_model(args.body)
 try:
     head = load_model(args.head)
@@ -21,8 +23,11 @@ except UnboundLocalError:
     head = load_model(args.head, custom_objects = {'binary_accuracy_svm': my_python.binary_accuracy_svm, 'hinge_svm': my_python.hinge_svm}) 
 noutput = head.layers[-1].output_shape[-1]
 from keras.layers import Dense, Activation
-body.add(Dense(noutput, name='head_dense'))
-body.layers[-1].set_weights(head.layers[-2].get_weights())
-body.add(Activation('sigmoid', name='head_activation'))
-body.compile(optimizer='sgd', loss='binary_crossentropy')
-body.save(args.out)
+
+x = body.layers[-1].output
+x = Dense(noutput, name='head_dense')(x)
+x = Activation('sigmoid', name='head_activation')(x)
+model = Model(inputs=body.layers[0].input, outputs=x)
+model.layers[-2].set_weights(head.layers[-2].get_weights())
+model.compile(optimizer='sgd', loss='binary_crossentropy')
+model.save(args.out)
