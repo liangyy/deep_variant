7827d419a7a77e048a44123c5a023f4219ab66f4
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-07-02 11:44:27 -0500

    updated script to generate random windows as well

--- motif_analysis/scripts/sliding_window_signal2region.R
+++ motif_analysis/scripts/sliding_window_signal2region.R
@@ -3,21 +3,21 @@ library("optparse")
 library(feather)
 
 option_list = list(
-    make_option(c("-f", "--file"), type="character", default=NULL, 
+    make_option(c("-f", "--file"), type="character", default=NULL,
                 help="dataset file name", metavar="character"),
-    make_option(c("-o", "--out"), type="character", default="out.txt", 
+    make_option(c("-o", "--out"), type="character", default="out.txt",
                 help="output file name [default= %default]", metavar="character"),
-    make_option(c('-m', '--mode'), type='character', default='positive',
-                help='match the mode with the input file (positive|negative)', metavar='character'),
+    make_option(c('-m', '--extract_mode'), type='character', default='positive',
+                help='match the mode with the input file (positive|negative) or set it to random', metavar='character'),
     make_option(c('-q', '--quantile'), type='double', default=0.0001)
-); 
+);
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
 
 pos <- read_feather(opt$file)
-if(opt$mode == 'positive'){
+if(opt$extract_mode == 'positive'){
     threshold <- quantile(unlist(pos), opt$quantile)
     seq <- c()
     position <- c()
@@ -27,7 +27,7 @@ if(opt$mode == 'positive'){
         seq <- c(seq, rep(x, length(pass.idx)))
         position <- c(position, pass.idx)
     }
-}else if(opt$mode == 'negative'){
+}else if(opt$extract_mode == 'negative'){
     threshold <- quantile(unlist(pos), 1 - opt$quantile)
     seq <- c()
     position <- c()
@@ -37,6 +37,16 @@ if(opt$mode == 'positive'){
         seq <- c(seq, rep(x, length(pass.idx)))
         position <- c(position, pass.idx)
     }
+}else if(opt$extract_mode == 'random'){
+  n <- opt$quantile * nrow(pos) * ncol(pos)
+  seq <- c()
+  position <- c()
+  for(x in 1 : n){
+      seq.idx <- sample(1 : nrow(pos), 1)
+      seq <- c(seq, pos)
+      pos.idx <- sample(1 : ncol(pos), 1)
+      position <- c(position, pass.idx)
+  }
 }
 
 out <- data.frame(seq_id = seq, pos_id = position)
