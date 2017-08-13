dba064a3aabd0e18c1fd4a46791851c7d259d47d
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-08-13 09:37:23 +0800

    updated

---
+++ motif_analysis_from_signal/scripts/bed_uppercase.py
@@ -0,0 +1,16 @@
+import argparse
+parser = argparse.ArgumentParser(prog='bed_uppercase.py', description='''
+    Convert all sequences to uppercase
+''')
+parser.add_argument('--in')
+parser.add_argument('--out')
+args = parser.parse_args()
+
+o = open(args.out, 'w')
+with open(args.in, 'r') as f:
+    for i in f:
+        if i[0] == '>':
+            o.write(i)
+        else:
+            o.write(i.upper())
+o.close()
